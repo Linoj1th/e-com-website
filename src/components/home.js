89094fd503c92bf805680da5ebe806e24fdfab52
Linoj1th <linolinojith0@gmail.com>
2024-09-13 01:29:51 +0530

    chore: feat:add product_type

--- src/components/home.js
+++ src/components/home.js
@@ -15,13 +15,49 @@ const Home = () => {
               {/* </BrowserRouter> */}
             </div>
             <div className='img_box'>
-                 <img src='./img/71kOH3in8TL._AC_SX466_-removebg-preview.png'></img>
+                 <img src='./img/71kOH3in8TL._AC_SX466_-removebg-preview.png' alt=''></img>
             </div>
         </div>
     </div> 
-    
+  <div className='product_type'>
+    <div className='container'>
+      <div className='box'>
+        <div className='img_box'>
+          <img src='./img/s24-removebg-preview.png' alt='mobil'></img>
+        </div>
+        <div className='detail'>
+          <p>27 products</p>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='img_box'>
+          <img src='./img/watch-removebg-preview.png'  alt='watch'></img>
+        </div>
+        <div className='detail'>
+          <p>12 products</p>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='img_box'>
+          <img src='./img/headset-removebg-preview.png' alt='headset'></img>
+        </div>
+        <div className='detail'>
+          <p>37 products</p>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='img_box'>
+          <img src='./img/drone-removebg-preview.png' alt='drone'></img>
+        </div>
+        <div className='detail'>
+          <p>8 products</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
     </>
   )
 }
 
-export default Home
+export default Home;
